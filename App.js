6c88f844de5582ec0fadf3352f297d326281e3b0
calebjsmith7 <calebjoelsmith@gmail.com>
2021-12-21 16:14:19 -0700

    processing power upgrade and new low tom sound

--- App.js
+++ App.js
@@ -14,97 +14,104 @@ import roto from './images/rotobg.jpg';
 import Hight from './sounds/hight.wav';
 import Midt1 from './sounds/midt1.wav';
 import Midt2 from './sounds/midt2.wav';
-import Lowt from './sounds/lowt.wav';
+import Lowt from './sounds/elowtom.wav';
 import Snare from './sounds/snare.wav';
 import Kick from './sounds/kick.wav';
 
 
 
 export default function App() {
- 
+
   React.useEffect(() => {
     SplashScreen.hide();
   });
 
 
 
- Sound.setCategory('Playback');
+  Sound.setCategory('Playback');
 
   var ht = new Sound(Hight);
   var htc = new Sound(Hight);
   var htcc = new Sound(Hight);
   var htccc = new Sound(Hight);
   var htcccc = new Sound(Hight);
+  var htccccc = new Sound(Hight);
   var mt1 = new Sound(Midt1);
   var mt1c = new Sound(Midt1);
   var mt1cc = new Sound(Midt1);
   var mt1ccc = new Sound(Midt1);
   var mt1cccc = new Sound(Midt1);
+  var mt1ccccc = new Sound(Midt1);
   var mt2 = new Sound(Midt2);
   var mt2c = new Sound(Midt2);
   var mt2cc = new Sound(Midt2);
   var mt2ccc = new Sound(Midt2);
   var mt2cccc = new Sound(Midt2);
+  var mt2ccccc = new Sound(Midt2);
   var lowt = new Sound(Lowt);
   var lowtc = new Sound(Lowt);
   var lowtcc = new Sound(Lowt);
   var lowtccc = new Sound(Lowt);
   var lowtcccc = new Sound(Lowt);
+  var lowtccccc = new Sound(Lowt);
   var snare = new Sound(Snare);
   var snarec = new Sound(Snare);
   var snarecc = new Sound(Snare);
   var snareccc = new Sound(Snare);
   var snarecccc = new Sound(Snare);
+  var snareccccc = new Sound(Snare);
   var kick = new Sound(Kick);
   var kickc = new Sound(Kick);
   var kickcc = new Sound(Kick);
   var kickccc = new Sound(Kick);
   var kickcccc = new Sound(Kick);
+  var kickccccc = new Sound(Kick);
   var current = undefined;
   var current2 = undefined;
   var current3 = undefined;
   var current4 = undefined;
   var current5 = undefined;
-  
+  var current6 = undefined;
 
 
- /* play = (note) => {
-    if(current != undefined){
-      current.stop();
-    }
-    current = note;
-    current.play((success) => {
-      if (success) {
-      } else {
-      }
-    }); 
-  } */
+
+  /* play = (note) => {
+     if(current != undefined){
+       current.stop();
+     }
+     current = note;
+     current.play((success) => {
+       if (success) {
+       } else {
+       }
+     }); 
+   } */
 
   play = (note) => {
     current = note;
-    note.play((success)=>{
-      if(success) {
+    note.play((success) => {
+      if (success) {
         current = undefined;
       }
     });
   }
 
   play2 = (note) => {
-    
+
     current2 = note;
-    note.play((success)=>{
-      if(success) {
-        
+    note.play((success) => {
+      if (success) {
+
         current2 = undefined;
       }
     });
   }
 
   play3 = (note) => {
-    
+
     current3 = note;
     note.play((success) => {
-      if(success){
+      if (success) {
         current3 = undefined;
       }
     })
@@ -112,7 +119,7 @@ export default function App() {
   play4 = (note) => {
     current4 = note;
     note.play((success) => {
-      if(success){
+      if (success) {
         current4 = undefined;
       }
     })
@@ -120,22 +127,30 @@ export default function App() {
   play5 = (note) => {
     current5 = note;
     note.play((success) => {
-      if(success){
+      if (success) {
         current5 = undefined;
+      }
+    })
+  }
+  play6 = (note) => {
+    current6 = note;
+    note.play((success) => {
+      if (success) {
+        current6 = undefined;
       }
     })
   }
 
   return (
     <View style={styles.sectionContainer}>
-      <ImageBackground source={roto} style={{flex: 1, justifyContent: 'center', width: Dimensions.get('window').width, height: Dimensions.get('window').height, alignSelf: "stretch", resizeMode: "cover", top: 0}}>
+      <ImageBackground source={roto} style={{ flex: 1, justifyContent: 'center', width: Dimensions.get('window').width, height: Dimensions.get('window').height, alignSelf: "stretch", resizeMode: "cover", top: 0 }}>
         <View style={styles.pads}>
-          <View onTouchStart={()=> current == undefined ? play(ht): current2 == undefined ? play2(htc) : current3 == undefined ? play3(htcc) : current4 == undefined ? play4(htccc) : play5(htcccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>HT</Text></TouchableOpacity></View>
-          <View onTouchStart={()=> current == undefined ? play(mt1): current2 == undefined ? play2(mt1c) : current3 == undefined ? play3(mt1cc) : current4 == undefined ? play4(mt1ccc) : play5(mt1cccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>MT</Text></TouchableOpacity></View>
-          <View onTouchStart={()=> current == undefined ? play(mt2): current2 == undefined ? play2(mt2c) : current3 == undefined ? play3(mt2cc) : current4 == undefined ? play4(mt2ccc) : play5(mt2cccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>MT</Text></TouchableOpacity></View>
-          <View onTouchStart={()=> current == undefined ? play(lowt): current2 == undefined ? play2(lowtc) : current3 == undefined ? play3(lowtcc) : current4 == undefined ? play4(lowtccc) : play5(lowtcccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>LT</Text></TouchableOpacity></View>
-          <View onTouchStart={()=> current == undefined ? play(snare): current2 == undefined ? play2(snarec) : current3 == undefined ? play3(snarecc) : current4 == undefined ? play4(snareccc) : play5(snarecccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>S</Text></TouchableOpacity></View>
-          <View onTouchStart={()=> current == undefined ? play(kick): current2 == undefined ? play2(kickc) : current3 == undefined ? play3(kickcc) : current4 == undefined ? play4(kickccc) : play5(kickcccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>K</Text></TouchableOpacity></View>
+          <View onTouchStart={() => current == undefined ? play(ht) : current2 == undefined ? play2(htc) : current3 == undefined ? play3(htcc) : current4 == undefined ? play4(htccc) : current5 == undefined ? play5(htcccc) : play6(htccccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>HT</Text></TouchableOpacity></View>
+          <View onTouchStart={() => current == undefined ? play(mt1) : current2 == undefined ? play2(mt1c) : current3 == undefined ? play3(mt1cc) : current4 == undefined ? play4(mt1ccc) : current5 == undefined ? play5(mt1cccc) : play6(mt1ccccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>MT</Text></TouchableOpacity></View>
+          <View onTouchStart={() => current == undefined ? play(mt2) : current2 == undefined ? play2(mt2c) : current3 == undefined ? play3(mt2cc) : current4 == undefined ? play4(mt2ccc) : current5 == undefined ? play5(mt2cccc) : play6(mt2ccccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>MT</Text></TouchableOpacity></View>
+          <View onTouchStart={() => current == undefined ? play(lowt) : current2 == undefined ? play2(lowtc) : current3 == undefined ? play3(lowtcc) : current4 == undefined ? play4(lowtccc) : current5 == undefined ? play5(lowtcccc) : play6(lowtccccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>LT</Text></TouchableOpacity></View>
+          <View onTouchStart={() => current == undefined ? play(snare) : current2 == undefined ? play2(snarec) : current3 == undefined ? play3(snarecc) : current4 == undefined ? play4(snareccc) : current5 == undefined ? play5(snarecccc) : play6(snareccccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>S</Text></TouchableOpacity></View>
+          <View onTouchStart={() => current == undefined ? play(kick) : current2 == undefined ? play2(kickc) : current3 == undefined ? play3(kickcc) : current4 == undefined ? play4(kickccc) : current5 == undefined ? play5(kickcccc) : play6(kickccccc)}><TouchableOpacity style={styles.pad}><Text style={styles.drumNames}>K</Text></TouchableOpacity></View>
         </View>
       </ImageBackground>
     </View>
@@ -144,11 +159,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   sectionContainer: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   height: Dimensions.get('screen').height,
-   width: Dimensions.get('screen').width
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: Dimensions.get('screen').height,
+    width: Dimensions.get('screen').width
   },
   pads: {
     display: 'flex',
